6adf93fe14d5c01ad610fa4b8123983a7c640447
MahirKothari <64363014+MahirKothari@users.noreply.github.com>
2020-05-24 13:38:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint;
+
+var bobObject1,bobObject2,bobObject3,bobObject4,bobObject5;
+var roof,rope1,rope2,rope3,rope4,rope5;
+
+
+function setup() {
+	createCanvas(800, 700);
+	engine = Engine.create();
+	world = engine.world;
+    
+    bobObject1 = new Bob(200,600,60);
+    bobObject2 = new Bob(300,600,60);
+    bobObject3 = new Bob(400,600,60);
+    bobObject4 = new Bob(500,600,60);
+    bobObject5 = new Bob(600,600,60);
+    roof = new Roof(300,10,600,20)
+    rope1 = new Rope(bobObject1.body,roof.body,-120,0);
+    rope2 = new Rope(bobObject2.body,roof.body,-60,0);
+    rope3 = new Rope(bobObject3.body,roof.body,0,0);
+    rope4 = new Rope(bobObject4.body,roof.body,60,0);
+    rope5 = new Rope(bobObject5.body,roof.body,120,0);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  background("grey");
+  Engine.update(engine)
+  
+  bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display();
+  roof.display();
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+  
+  drawSprites();
+ 
+}
+
+
+
